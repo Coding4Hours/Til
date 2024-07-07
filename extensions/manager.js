f62c3ef9364a54501e0669346dc89b0b66908e21
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-07 14:42:39 -0700

    Create manager.js

---
+++ extensions/manager.js
@@ -0,0 +1,28 @@
+chrome.management.getAll(function(extensions) {
+  var options = [];
+  extensions.forEach(function(extension) {
+    if (extension.id !== chrome.runtime.id) {
+      options.push(extension.name);
+    }
+  });
+  var choice = prompt("Choose an extension to manage:\\n" + options.join("\\n"));
+  var selectedExtension = extensions.find(function(ext) {
+    return ext.name === choice;
+  });
+  if (selectedExtension) {
+    var action = selectedExtension.enabled ? 'disable' : 'enable';
+    var confirmAction = confirm('Do you want to ' + action + ' the extension: ' + selectedExtension.name + '?');
+    if (confirmAction) {
+      chrome.management.setEnabled(selectedExtension.id, !selectedExtension.enabled, function() {
+        if (chrome.runtime.lastError) {
+          console.error('Failed to ' + action + ' extension ' + selectedExtension.name + ': ' + chrome.runtime.lastError.message);
+        } else {
+          refreshExtensionList();
+          openWebStore();
+        }
+      });
+    }
+  } else {
+    alert('Invalid choice');
+  }
+});
