9cd35dc5bc54a0edb4c6db3ebada67ff349e6027
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-07 15:58:02 -0700

    Update build_database.py

--- build_database.py
+++ build_database.py
@@ -3,9 +3,7 @@ import git
 import pathlib
 import sqlite_utils
 
-
 root = pathlib.Path(__file__).parent.resolve()
-
 
 def created_changed_times(repo_path, ref="master"):
     created_changed_times = {}
@@ -26,20 +24,18 @@ def created_changed_times(repo_path, ref="master"):
                     "updated_utc": dt.astimezone(timezone.utc).isoformat(),
                 }
             )
-            
     return created_changed_times
-
 
 def build_database(repo_path):
     all_times = created_changed_times(repo_path)
     db = sqlite_utils.Database(repo_path / "til.db")
     table = db.table("til", pk="path")
     for filepath in root.glob("*/*.md"):
-        fp = filepath.open()
-        title = fp.readline().lstrip("#").strip()
-        body = fp.read().strip()
+        with filepath.open() as fp:
+            title = fp.readline().lstrip("#").strip()
+            body = fp.read().strip()
         path = str(filepath.relative_to(root))
-        url = "https://github.com/Coding4Hours/til/blob/master/{}".format(path)
+        url = f"https://github.com/Coding4Hours/til/blob/master/{path}"
         record = {
             "path": path.replace("/", "_"),
             "topic": path.split("/")[0],
@@ -48,10 +44,9 @@ def build_database(repo_path):
             "body": body,
         }
         record.update(all_times[path])
-        table.insert(record)
+        table.insert(record, replace=True)
     if "til_fts" not in db.table_names():
         table.enable_fts(["title", "body"])
-
 
 if __name__ == "__main__":
     build_database(root)
