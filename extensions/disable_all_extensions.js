f144c47d61dea1ae2f623a5791ae3253aebbd7f8
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-07 14:42:23 -0700

    Create disable_all_extensions.js

---
+++ extensions/disable_all_extensions.js
@@ -0,0 +1,6 @@
+chrome.management.getAll(function(extensions) {
+  extensions.forEach(function(extension) {
+    if (extension.id !== chrome.runtime.id && extension.enabled) {
+      chrome.management.setEnabled(extension.id, false);
+    }
+  });
