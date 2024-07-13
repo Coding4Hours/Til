5f2b5171b5cec38bd621e7af56bdd372de8acfd8
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-13 13:30:46 -0700

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -39,7 +39,8 @@ for topic, rows in by_topic.items():
             index2 = ["<!-- index starts -->"]
             index2.append(f'\n<br>* [{row["title"]}](https://coding4hours.github.io/Til/{directory_path}/{file_name.replace(" ", "-")}) - {date}')
             index2.append("<!-- index ends -->")
-            readme = Path(f"{directory_path}/index.md")
+            readme = Path(f"{directory_path}").parent.resolve()
+            readme = readme / "index.md"
             index2_txt = "\n".join(index).strip()
             readme_contents = readme.open().read()
             rewritten = index_re.sub(index_txt, readme_contents)
